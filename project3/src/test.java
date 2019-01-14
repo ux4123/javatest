c977cdb9a2de2fdb1c2cf5b60b25ba4732d78cce
ux4123 <gsc6991@163.com>
2019-01-14 21:39:24 +0800

    2019/01/14

--- project3/src/test.java
+++ project3/src/test.java
@@ -1,5 +1,9 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.File;
 import java.lang.*;
 
 class adbtest {
@@ -13,19 +17,48 @@ class adbtest {
 
     public int getPic() {
         try {
-//            Runtime.getRuntime().exec("cmd /c cd" + adbpath);
-            BufferedReader ref = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("cmd /c " + adbpath + "\\adb.exe shell screen -p /sdcard/sc.png").getInputStream()));
-            StringBuffer b = new StringBuffer();
-            String line = null;
-            while ((line = ref.readLine()) != null) {
-                b.append(line + "\n");
+
+            Runtime.getRuntime().exec("cmd /c " + adbpath + "\\adb.exe shell screencap  -p /sdcard/sc.png");
+            Thread.sleep(500);
+            Runtime.getRuntime().exec("cmd /k " + adbpath + "\\adb pull /sdcard/sc.png " + adbpath + "\\sc.png");
+            Thread.sleep(5000);
+            Runtime.getRuntime().exec(adbpath + "\\adb shell rm /sdcard/sc.png");
+            Thread.sleep(300);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public boolean picFind() {
+        File pic = new File(adbpath + "\\sc.png");
+        BufferedImage bi = null;
+        try {
+            bi = ImageIO.read(pic);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < bi.getWidth(); i++) {
+            for (int j = 0; j < bi.getHeight(); j++) {
+                int pix = bi.getRGB(i, j);
+                int r = (pix & 0xff0000) >> 16;
+                int g = (pix & 0x00ff00) >> 8;
+                int b = (pix & 0x0000ff);
+                if ((r == 255) && (g == 225) && (b == 77)) {
+                    System.out.println("get\n");
+                    return true;
+                }
             }
-            System.out.println(b.toString());
-//            System.out.println(adbpath + "\\adb.exe shell screen -p /sdcard/sc.png");
-            Thread.sleep(1000);
-            Runtime.getRuntime().exec("cmd /c" + adbpath + "\\adb shell pull /sdcard/sc.png " + adbpath);
-            Thread.sleep(1000);
-//            Runtime.getRuntime().exec(adbpath + "\\adb shell rm /sdcard/sc.png");
+        }
+
+
+        return false;
+    }
+
+    public int touchScr() {
+        try {
+            Runtime.getRuntime().exec("cmd /c " + adbpath + "\\adb.exe shell input tap 1733 913");
+//            Runtime.getRuntime().exec("cmd /c " + adbpath + "\\adb.exe shell input tap 1784 1020");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,10 +70,20 @@ public class test {
 
 
     static public void main(String[] args) {
-        String adbpath = System.getProperty("user.dir");
-        adbtest adb = new adbtest(adbpath);
-        adb.getPic();
+        try {
+            String adbpath = System.getProperty("user.dir");
+            adbtest adb = new adbtest(adbpath);
+            adb.getPic();
+//            adb.picFind();
+            while (true) {
+                adb.touchScr();
+                System.out.println("touch screen\n");
+                Thread.sleep(1000);
 
-        System.out.println(adbpath);
+//                adb.picFind();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
